4fca265722c7c2aeab672a0595154aad5aebf847
AmirePacolli <amirepacolli@gmail.com>
2017-04-18 23:04:38 +0200

    Create FIEK-TCP-Klienti.py

---
+++ FIEK-TCP-Klienti.py
@@ -0,0 +1,37 @@
+from socket import *
+import sys
+print("Klienti TCP")
+
+serverName = "127.0.0.1"
+serverPort = 9000
+print('Serveri eshte ne linje...')
+print('')
+print('Metodat e mundshme: ')
+print('IP - \t\tkthen  IP adresen e klientit')
+print('PORT - \t\tkthen protin e klientit') 
+print('ZANORE - \tkthen numrin e zanoreve ne nje fjale') 
+print('PRINTO - \tkthen fjaline e derguar') 
+print('HOST - \t\tkthen emrin e hostit') 
+print('TIME- \t\tkthen kohen aktuale ne server') 
+print('KENO - \t\tkthen 20 numra nga rangu[1,80]') 
+print('FAKTORIEL - \tgjen faktorielin e parametrit hyres')
+print('KONVERTO - \tkthen si rezultat konvertimin e opioneve varesisht opsionit te zgjedhur: \n\t\t\tCelsiusToKelvin\n\t\t\tCelsiusToFarenheit\n\t\t\tKelvinToFarenheit\n\t\t\tKelvinToCelsius\n\t\t\tFarenheitToCelcius\n\t\t\tFarenheitToKelvin\n\t\t\tPoundToKilogram\n\t\t\tKilogramToPound')
+print('KODI - \tenkripton fjaline e shenuar sipas kodit te Cezarit')
+print('PRIMAR - \ttregon nese numri i shenuar eshte numer i thjeshte')
+print('SIPERFAQJA - \tkalkulon siperfaqen e rrethit me rrezen e dhene')
+print('PERIMETRI - \tkalkulon perimetrin e rrethit me rrezen e dhene')
+print('ASTRO - \ttregon shenjen e horoskopit ne baze te datelindjes')
+print('GJATESIA - \ttregon gjatesine e tekstit te dhene')
+print('ODD - \tshtyp shkronjat me index cift')
+print('BINARY - \tkthen numrin e dhene binar ne numer decimal')
+print('INDEX - \ttregon indeksat dhe shkronjat perkatese te nje teksti te dhene')
+print('DEGREES -\tshnderron shkallet e dhena ne radian')
+while True:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+    mesazhi = input("Shkruani ndonje funksion: ")
+    clientSocket.send(mesazhi.encode("ASCII"))
+    message = clientSocket.recv(128)
+    print("Rezultati: ", message.decode("ASCII"))
+    
+    
