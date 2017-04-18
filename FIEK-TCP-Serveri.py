a555912c0c47310423030119694b06967b5efb07
fidianamujaj <fidianamujaj@hotmail.com>
2017-04-18 23:11:08 +0200

    Update FIEK-TCP-Serveri.py
    Shtimi i metodave KENO dhe FAKTORIEL

--- FIEK-TCP-Serveri.py
+++ FIEK-TCP-Serveri.py
@@ -18,17 +18,39 @@ while 1:
     message = conS.recv(128)
     mesazhi = message.decode("ASCII").split(' ')
     
-    def ZANORE():
-    nrZ=0
-    zanoret = ['A', 'E', 'I', 'O', 'U', 'Y','a', 'e', 'i', 'o', 'u', 'y']
-        for i in opsioni:
-              if i in zanoret:
-                  nrZ+=1
-                  conS.send(str(str(nrZ)).encode("ASCII"))
-                  conS.close()
-    def PRINTO():
-         conS.send(str(opsioni).encode("ASCII"))
+    def ZANORE(teksti):
+        nrZ=0
+        zanoret = ['A', 'E', 'I', 'O', 'U', 'Y','a', 'e', 'i', 'o', 'u', 'y']
+        for i in teksti:
+            if i in zanoret:
+                nrZ+=1
+        serverSocket.sendto(str(str(nrZ)).encode('utf-8'), clientAddress)
+
+    def PRINTO(teksti):
+        serverSocket.sendto(str(teksti).encode('utf-8'), clientAddress)
     def HOST():
-         hosti = gethostbyname(gethostname())
-         conS.send(str("Hosti " + hosti).encode("ASCII"))
-         conS.close()
+                  hosti = gethostbyname(gethostname())
+                  conS.send(str("Hosti " + hosti).encode("ASCII"))
+                  conS.close()
+    def KENO():
+                  keno=[random.randint(1,80) for i in range(20)]
+                  conS.send(str(str(keno)).encode("ASCII"))
+                  conS.close()
+    def FAKTORIEL():
+                f=1
+                if(opsioni==""):
+                   conS.send(str("Ju lutemi shtypni nje numri pas komandes.").encode('ASCII'))
+                else:
+                  numri = int(opsioni)
+                if numri < 0:
+                   conS.send(str("Faktorieli nuk i perkrah numrat negativ").encode('ASCII'))
+                elif numri == 0:
+                   conS.send(str("Faktorieli i numrit 0 eshte 1").encode('ASCII'))
+                elif numri > 1:
+                     f= 1
+                while numri > 1:
+                  f = f* numri
+                  numri = numri - 1 
+                  
+                conS.send(str("Faktorieli i numrit " + str(opsioni) + " eshte " + str(f) + ".").encode('ASCII'))
+                conS.close() 
